17e8b8f9887ff8e665bc1392875dda27a0d9ed34
BrezeanuGabriela <100274462+BrezeanuGabriela@users.noreply.github.com>
2022-12-06 01:07:12 +0200

    Update main.m

--- main.m
+++ main.m
@@ -2,3 +2,16 @@ path = "E:\FACULTATE\Anul 4\Sem1\MPS\Project\arhive\archive\wine\winequalityN.cs
 [input, output] = ReadData(path);
 output_column_name = output.Properties.VariableNames;
 input_column_names = input.Properties.VariableNames;
+
+for i=2:1:width(input)
+    in = input{:,[input_column_names(i)]};
+    is_nan = isnan(in);
+    disp(sum(is_nan))
+end
+
+
+% table column -> array i guess
+out = output{:, ["quality"]};
+histogram(out);
+wine_types = unique(input(:,"type"));
+
