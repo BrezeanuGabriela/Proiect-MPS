5c5a7b32b7419a8ff15604d387677dfe6b276676
BrezeanuGabriela <100274462+BrezeanuGabriela@users.noreply.github.com>
2022-12-06 00:44:09 +0200

    Update main.m

--- main.m
+++ main.m
@@ -1,2 +1,4 @@
 path = "E:\FACULTATE\Anul 4\Sem1\MPS\Project\arhive\archive\wine\winequalityN.csv";
 [input, output] = ReadData(path);
+output_column_name = output.Properties.VariableNames;
+input_column_names = input.Properties.VariableNames;
